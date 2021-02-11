a775f33f292a0cb1e18e5a8cb282f6af38098ef6
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2021-02-11 09:54:05 -0700

    add programs and watch online

---
+++ src/pages/programs.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import HeroBanner from '../components/heroBanner'
+import Container from '../components/container'
+import ProgramList from '../components/programList'
+
+const ImgContainer = styled.div`
+  width: 100%;
+  max-width: 250px;
+  padding: 20px 0;
+`
+const StyledImg = styled(Img)`
+  width: 100%;
+`
+const ProgramPage = ({ data }) => (
+  <Layout>
+    <SEO title="Programs" />
+    <HeroBanner>
+      <ImgContainer>
+        <StyledImg fluid={data.wwLogo.childCloudinaryAsset.fluid} />
+      </ImgContainer>
+      <h1>Programs</h1>
+    </HeroBanner>
+
+    <Container>
+      <h3 style={{ marginTop: 20 }}>Get the programs for this series here!</h3>
+      <ProgramList programs={data.dataJson.programs} />
+    </Container>
+  </Layout>
+)
+
+export default ProgramPage
+
+export const query = graphql`
+  query ProgramQuery {
+    dataJson {
+      programs {
+        date
+        location
+      }
+    }
+    wwLogo: file(name: { eq: "wwLogo" }) {
+      childCloudinaryAsset {
+        fluid {
+          ...CloudinaryAssetFluid
+        }
+      }
+    }
+  }
+`
